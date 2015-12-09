3cee52defc331cc3bbe551b5db9e4e87f6c51c70
Robotato <michaelyang42@gmail.com>
2015-12-09 03:30:57 -0500

    initial commit
    Hello World

---
+++ Network/Enzype.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Enzypes.Network
+{
+    public class Enzype : Linkable
+    {
+        /// <summary>
+        /// Must be in interval [0, 1].
+        /// </summary>
+        public double Active_Fraction { get; set; }
+
+        /// <summary>
+        /// Must be >= 0.
+        /// </summary>
+        public double Number { get; set; }
+
+        public Enzype(double activeFraction, double number)
+        {
+            Active_Fraction = activeFraction;
+            Number = number;
+        }
+    }
+}
---
+++ Network/NetworkController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Enzypes.Network.Scripting;
+using Enzypes.Network.Utilities;
+
+namespace Enzypes.Network
+{
+    public class NetworkController
+    {
+        public List<RunScript> RunScripts { get; set; }
+        public List<HeldAction> CurrentHeldActions { get; set; }
+        public double Time { get; private set; }
+
+        public NetworkController()
+        {
+            RunScripts = new List<RunScript>();
+            CurrentHeldActions = new List<HeldAction>();
+        }
+
+        public void Update(NetworkState networkState, double delta_t)
+        {
+            //TODO: update scripts
+
+
+            //production
+            for (int i = 0; i < networkState.Operons.Count; i++)
+            {
+                Operon o = networkState.Operons[i];
+                CircularList<Enzype> enzypes = networkState.GetEnzypes(i);
+
+                for (int k = 0; k < enzypes.Count; k++)
+                {
+                    Enzype e = enzypes[k];
+
+                    //check if N is being held constant
+                    bool N_held = false;
+                    foreach (HeldAction h in CurrentHeldActions)
+                    {
+                        if (h.TargetIndex == k && h.TargetProperty == Scripting.Action.ActionTargetProperty.N_k)
+                            N_held = true;
+                    }
+
+                    if (!N_held)
+                        e.Number += Bounds.RestrictLower(o.P_current * delta_t, 0);
+                    
+                    Console.WriteLine(e.Number);
+                }
+            }
+
+            Time += delta_t;
+        }
+
+        public void ResetTime()
+        {
+            Time = 0;
+        }
+    }
+}
